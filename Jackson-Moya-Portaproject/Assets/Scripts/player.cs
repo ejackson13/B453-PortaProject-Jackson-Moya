5dd9356e916507400b776f6f73c1a44a248f2f06
ejackson13 <evjack1393@gmail.com>
2024-03-21 22:33:05 -0400

    Finished basic player movement

--- Jackson-Moya-Portaproject/Assets/Scripts/player.cs
+++ Jackson-Moya-Portaproject/Assets/Scripts/player.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.U2D;
@@ -24,16 +26,15 @@ public class player : MonoBehaviour
     private PlayerState pState = PlayerState.State_normal;
 
     public bool flag_constant_spritetrail = false;
-    public Color color_spritetrail;
+    public UnityEngine.Color color_spritetrail;
     private bool flag_can_create_spritetrail = true;
 
     private const float lerp_constant = 0.5f;
-    private Vector2 vector_gravity_up = new Vector2(0, 10); // should be const
-    private Vector2 vector_gravity_down = new Vector2(0, 12.5f); // should be const
+    [SerializeField] private Vector2 vector_gravity_up = new Vector2(0, -10); // should be const
+    [SerializeField] private Vector2 vector_gravity_down = new Vector2(0, -12.5f); // should be const
 
-    private Vector2 vector_normal = new Vector2(0, -1); // should be const
-    private const float maximum_speed = 95;
-    private const float jump_force = 175;
+    [SerializeField] private float maximum_speed = 95;
+    [SerializeField] private float jump_force = 175;
 
     private float airTime = 0;
     private const float maxAirTime = 5;
@@ -49,7 +50,7 @@ public class player : MonoBehaviour
     //const fxPlayerJumpDust = preload("res://Scenes/fxPlayerJumpDust.tscn")
     //const fxPlayerLandDust = preload("res://Scenes/fxPlayerLandDust.tscn")
 
-    private SpriteRenderer nSprite; // sprite type instead?
+    [SerializeField] private SpriteRenderer nSprite; // sprite type instead?
     //onready var nSprEyes:Sprite = $eyes
     //onready var nAnimationPlayer:AnimationPlayer = $animation_player
     //onready var nDiveAim:Node2D =$dive_aim/dive_aim
@@ -61,9 +62,7 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //add_to_group('Player')   // like giving object tag player
         //nSprEyes.visible = false
-        //global.player = self  // unnecessary i believe
         //nVignette.modulate.a = 0
         //nDiveAim.rotation = 0.0 if self.last_horizontal_direction == 1 else -PI
     }
@@ -71,22 +70,22 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        nSprite.flipY = last_horizontal_direction != 1;
+        nSprite.flipX = last_horizontal_direction != 1;
 
 
         if (pState == PlayerState.State_dive)
         {
             anim = "idle";
         }
-        else 
-        { 
-		    if (is_on_floor())
+        else
+        {
+            if (is_on_floor())
             {
                 anim = Mathf.Abs(vectorVelocity.x) <= 10 ? "idle" : "walk";
             }
-            else 
+            else
             {
-                anim = vectorVelocity.y < 0 ? "going_up" : "going_down";
+                anim = vectorVelocity.y < 0 ? "going_down" : "going_up";
             }
         }
 
@@ -107,14 +106,41 @@ public class player : MonoBehaviour
 
 
         //Vector2 vector_direction_input = new Vector2(1 if Input.is_action_pressed('ui_right') else -1 if Input.is_action_pressed('ui_left') else 0, 1 if Input.is_action_pressed('ui_down') else -1 if Input.is_action_pressed('ui_up') else 0);
-        Vector2 vector_direction_input = new Vector2(Input.GetAxis("horizontal"), Input.GetAxis("vertical") * -1); // vertical input seems to be inverted for some reason
+        //Vector2 vector_direction_input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 vector_direction_input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            vector_direction_input.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            vector_direction_input.x = 1;
+        }
+        else
+        {
+            vector_direction_input.x = 0;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            vector_direction_input.y = -1;
+        }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            vector_direction_input.y = 1;
+        }
+        else
+        {
+            vector_direction_input.y = 0;
+        }
+
         last_horizontal_direction = vector_direction_input.x != 0 ? vector_direction_input.x : last_horizontal_direction;
 	
 	    if (active)
         {
 		    if (pState == PlayerState.State_normal) 
             {
-                _state_normal(delta, vector_direction_input);
+                _state_normal(Time.deltaTime, vector_direction_input);
             }
             /*
              * Diving
@@ -134,14 +160,174 @@ public class player : MonoBehaviour
         */
     }
 
+    private void _state_normal(float _delta, Vector2 vector_direction_input)
+    {
+
+        // I believe this is for the visual effect that appears when the player enters a block
+        //nVignette.modulate.a = lerp(nVignette.modulate.a, 0, 0.1);
+
+        //set_collision_layer_bit(0, true)
+
+        //set_collision_mask_bit(0, true)
+
+        if (vectorVelocity.y > 0 && Input.GetKey(KeyCode.Z)) 
+        {
+            vector_gravity = vector_gravity_up;
+        } 
+        else if (is_on_floor())
+        {
+            vector_gravity = Vector2.zero;
+        }
+        else
+        {
+            vector_gravity = vector_gravity_down;
+        }
+
+        // sets buffers for jump and dive inputs
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+		    jumpBuffer = maxJumpBuffer;
+        }
+
+        /*
+         * Dive mechanic
+        if (Input.GetKeyDown(KeyCode.X)) 
+        {
+            diveBuffer = maxDiveBuffer;
+        }
+        */
+
+
+        if (is_on_floor()) 
+        {
+            airTime = maxAirTime;
+        }
+        else 
+        {
+            airTime -= 0.5f;
+        }
+
+        jumpBuffer -= 0.5f; ;
+        //diveBuffer -= 0.5f;
+
+
+        if (jumpBuffer > 0 && airTime > 0)
+        {
+            airTime = 0;
+            jumpBuffer = 0;
+
+            // animation
+            //_twn_squishy()
+
+            // jump sound effect
+            //$sounds / snd_jump.play()
+
+            vectorVelocity.y = jump_force;
+
+            // for particle effects
+            //var i = fxPlayerJumpDust.instance();
+            //i.global_position = self.global_position - vSpriteOffset
+            //get_parent().add_child(i)
+        }
+
+
+        /*
+         * For diving into blocks
+        if (diveBuffer > 0 && dive_aim.get_overlapping_bodies().size() > 0)
+        { 
+
+            var phaseable = true
+		    #Check if the tileset is unphaseable
+		    for body in dive_aim.get_overlapping_bodies():
+
+                if body.is_in_group('Unphaseable'):
+
+                    phaseable = false
+
+
+            if phaseable:
+			    global.changeToLowPassMusic()
+
+
+                if randf() <= 0.5:
+				    $sounds / snd_dive.play()
+
+                else:
+				    $sounds / snd_dive1.play()
+
+                self.state = State_dive
+                var cursor_position = dive_aim.get_node('position_2d').global_position#-Vector2(0,1)
+			    var vector_target_position = Vector2(
+                    floor(cursor_position.x / global.tile_size.x) * global.tile_size.x,\
+
+                    floor(cursor_position.y / global.tile_size.y) * global.tile_size.y \
+				    ) + global.tile_size / 2 + vSpriteOffset
+
+
+                var _v = nTwnDive.interpolate_property(self, 'global_position', self.global_position, vector_target_position, twn_duration, Tween.TRANS_QUART, Tween.EASE_OUT)
+
+                _v = nTwnDive.start()
+
+                nAnimationPlayer.play("idle")
+
+                create_splash(20, 30, -(self.global_position - vector_target_position), (self.global_position - vector_target_position) / 2)
+
+                createSpherize(vector_target_position)
+			    $camera2D.minorShake()
+
+                return;
+        }
+        */
+
+        var initialVelocity = vectorVelocity;
+
+        //Debug.Log(vector_direction_input);
+        vectorVelocity.x = Mathf.Lerp(vectorVelocity.x, maximum_speed * vector_direction_input.x, lerp_constant);
+
+        if (vectorVelocity.y <= 0 && is_on_floor())
+        {
+            vectorVelocity.y = 0;
+        } 
+        vectorVelocity.y += vector_gravity.y * Time.deltaTime;
+
+        GetComponent<Rigidbody2D>().velocity = vectorVelocity;
+
+        /*
+         * landing sound effect
+        if (initialVelocity.y != vectorVelocity.y and self.is_on_floor())
+        { 
+		    $sounds / snd_land.play()
+        }
+        */
+
+        /*
+         * Particle effects and animation
+        if !self.bWasOnFloor and self.is_on_floor():
+		    var i = fxPlayerLandDust.instance()
+
+            i.global_position = self.global_position - vSpriteOffset
+            get_parent().add_child(i)
+
+            tweenLandSquish()
+        */
+
+        bWasOnFloor = is_on_floor();
+    }
 
     private bool is_on_floor()
     {
         // check if the bottom of the collision box contacts the ground
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - GetComponent<BoxCollider2D>().size.y / 2), Vector2.down, .01f);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector2(transform.position.x, transform.position.y), Vector2.down, (GetComponent<BoxCollider2D>().size.y / 2) + .05f);
 
-        // may need to update to tell difference between colliders, will come back when tags for ground objects and whatnot have been decided upon
-        return hit.transform != null;
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.gameObject.CompareTag("Ground"))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
